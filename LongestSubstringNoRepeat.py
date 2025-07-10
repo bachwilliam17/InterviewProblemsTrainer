db83ffda0d91d7d5e7ae49fd4b01925a9987d893
unknown <bachwilliam17@gmail.com>
2025-07-10 19:48:44 -0400

    Longest Substring without repetition Solution

--- LongestSubstringNoRepeat.py
+++ LongestSubstringNoRepeat.py
@@ -1,28 +1,24 @@
+# Solution : https://leetcode.com/problems/longest-substring-without-repeating-characters/solutions/3649636/3-method-s-c-java-python-beginner-friendly/
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        current_substring = {}
+        current_substring = set()
+        max_length = 0 
+        left_pointer = 0
 
-        current_max = 0
-        max_seen = 0
-
-        for index, letter in enumerate(s) :
-
-            if letter in current_substring : 
-                if len(current_substring) > current_max : 
-                    max_seen = len(current_substring)
-                current_max = 0
-                current_substring = {}
-                continue
-
+        for right_pointer in range(len(s)): 
+            if s[right_pointer] not in current_substring : 
+                current_substring.add(s[right_pointer])
+                max_length = max(max_length, right_pointer - left_pointer + 1)
+            
             else : 
-                current_max += 1
-                current_substring[index] = letter
+                while s[right_pointer] in current_substring : 
+                    current_substring.remove(s[left_pointer])
+                    left_pointer += 1
+                current_substring.add(s[right_pointer])
 
-        if current_max > max_seen : 
-            max_seen = current_max
-        
-        return max_seen
+        return max_length
