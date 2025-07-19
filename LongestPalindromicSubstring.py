a3a357d8661c961d0ebc52a89d3ce3d502e65245
unknown <bachwilliam17@gmail.com>
2025-07-19 00:03:21 -0400

    Longest Palindromic Substring sol start

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # let's go
+        n = len(s)
+
+        dp = [[False] * n for _ in range(n)]
+
+        for length in range(2, n + 1) : 
+            for i in range(n) 
