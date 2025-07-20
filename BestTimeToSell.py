35b007b36d7c43d64f12df3d4bcb67947aa06864
unknown <bachwilliam17@gmail.com>
2025-07-19 21:37:49 -0400

    Best time to buy and sell stock - Solution

--- BestTimeToSell.py
+++ BestTimeToSell.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices) :
+            if prices[r] < prices[l] : 
+                l = r 
+
+            else :  
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+
+            r += 1 
         
+        return max_profit
