327be24177239fbc8be285d4103f43c4a0800fed
unknown <bachwilliam17@gmail.com>
2025-07-25 22:01:43 -0400

    2 problems solved

--- MaximumProductSubarray.py
+++ MaximumProductSubarray.py
@@ -6,3 +6,14 @@ class Solution(object):
         """
         
         # use current min and max
+        cur_min, cur_max = 1, 1
+        sol = max(nums)
+
+        for num in nums : 
+            temp = cur_min * num
+            cur_min = min(cur_min * num, cur_max * num, num)
+            cur_max = max(temp, cur_max * num, num)
+
+            sol = max(sol, cur_max)
+
+        return sol
---
+++ MinInRotatedSortedArray.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        l, r = 0, len(nums) - 1
+        m = len(nums) // 2 
+
+        while True : 
+            if m == 0 : 
+                return nums[m]
+            
+            if nums[m - 1] < nums[m] : 
+                if nums[m] > nums[r] : 
+                    l = m + 1
+                else : 
+                    r = m - 1
+
+            else : 
+                return nums[m]
+
+            m = (l + r) // 2 
