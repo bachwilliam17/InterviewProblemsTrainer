2588b6800c0e612cdd5b1c3bb9e41c85dc0d4b35
unknown <bachwilliam17@gmail.com>
2025-08-06 22:13:16 -0400

    Combination sum - solved

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -11,3 +11,5 @@ class Solution(object):
             for j in range(i+1, len(nums)) : 
                 if nums[i] < nums[j] : 
                     dp[i] = max(dp[i], 1 + dp[j])
+
+        return max(dp)
