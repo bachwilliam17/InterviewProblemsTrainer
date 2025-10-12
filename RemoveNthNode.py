49f2467738289ca68d50fd84de99acfedabc6bdf
unknown <bachwilliam17@gmail.com>
2025-10-12 16:41:59 -0400

    Remove Nth node - solved

--- RemoveNthNode.py
+++ RemoveNthNode.py
@@ -11,4 +11,17 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head 
+
+        while n > 0 : 
+            right = right.next
+            n -= 1
+
+        while right : 
+            right = right.next
+            left = left.next
+
+        left.next = left.next.next
+        return dummy.next
