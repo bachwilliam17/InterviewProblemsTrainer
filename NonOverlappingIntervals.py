a04acb4c242a06b14c6410ddf7677fb39490714a
unknown <bachwilliam17@gmail.com>
2025-10-13 21:14:43 -0400

    Non-overlapping intervals - solved

---
+++ NonOverlappingIntervals.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        
+        intervals = intervals.sort()
+
+        prevEnd = intervals[0][1]
+        res = 0
+        for start, end in intervals[1:] :
+            if start >= prevEnd : 
+                prevEnd = end
+            else : 
+                res += 1 
+                prevEnd = min(end, prevEnd)
+        
+        return res
